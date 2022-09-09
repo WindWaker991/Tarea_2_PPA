b547b9c0e34800b07e9afa57a973dff05480705f
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2022-09-09 18:08:12 -0300

    Agregado las funciones para cada botón, falta agregar condicionales

--- Actividad Operaciones PPA/src/interfaz/Calculadora.java
+++ Actividad Operaciones PPA/src/interfaz/Calculadora.java
@@ -8,7 +8,10 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Calculadora extends JFrame {
 
@@ -62,18 +65,55 @@ public class Calculadora extends JFrame {
 		contentPane.add(lblSegundoNmero);
 		
 		JButton btnNewButton = new JButton("+");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textField.getText().equals("") || textField_1.getText().equals("")) {
+					JFrame jFrame = new JFrame();
+					JOptionPane.showMessageDialog(jFrame, "Debes ingresar valores a los dos campos.");
+					// AGREGAR CONDICIONALES DE SUMA
+				}
+			}
+		});
 		btnNewButton.setBounds(50, 173, 85, 21);
 		contentPane.add(btnNewButton);
 		
+		
 		JButton btnNewButton_1 = new JButton("-");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textField.getText().equals("") || textField_1.getText().equals("")) {
+					JFrame jFrame = new JFrame();
+					JOptionPane.showMessageDialog(jFrame, "Debes ingresar valores a los dos campos.");
+					// AGREGAR CONDICIONALES DE RESTA
+				}
+			}
+		});
 		btnNewButton_1.setBounds(165, 173, 85, 21);
 		contentPane.add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("*");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textField.getText().equals("") || textField_1.getText().equals("")) {
+					JFrame jFrame = new JFrame();
+					JOptionPane.showMessageDialog(jFrame, "Debes ingresar valores a los dos campos.");
+					// AGREGAR CONDICIONALES DE MULTIPLICACIÓN
+				}
+			}
+		});
 		btnNewButton_2.setBounds(50, 208, 85, 21);
 		contentPane.add(btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("/");
+		btnNewButton_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (textField.getText().equals("") || textField_1.getText().equals("")) {
+					JFrame jFrame = new JFrame();
+					JOptionPane.showMessageDialog(jFrame, "Debes ingresar valores a los dos campos.");
+					// AGREGAR CONDICIONALES DE DIVISION
+				}
+			}
+		});
 		btnNewButton_3.setBounds(165, 208, 85, 21);
 		contentPane.add(btnNewButton_3);
 	}
