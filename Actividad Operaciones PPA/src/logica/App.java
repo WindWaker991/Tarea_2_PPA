ff58975b6d5336f6a16bdf1ce82a417001a0eeab
Kevin Alfaro <kevin.alfaro@alumnos.ucn.cl>
2022-09-09 17:51:35 -0300

    Agregado main 'App.java', se puede ejecutar pero sólo está disponible la interfaz

--- Actividad Operaciones PPA/src/logica/App.java
+++ Actividad Operaciones PPA/src/logica/App.java
@@ -1,5 +1,14 @@
 package logica;
 
+import interfaz.Calculadora;
+
 public class App {
+
+	public static void main(String[] args) {
+		
+		Calculadora ventana = new Calculadora();
+		ventana.setVisible(true);
+
+	}
 
 }
